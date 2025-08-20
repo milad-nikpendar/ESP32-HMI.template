ac0101dd2bd17c124d49124537b9df42c17eb2cc
Milad Nikpendar <milad82nikpendar@gmail.com>
2025-08-20 21:34:20 +0330

    lvgl_touch_gt911.h
    Signed-off-by: Milad Nikpendar <milad82nikpendar@gmail.com>

--- esp32-hmi/lvgl_touch_gt911.h
+++ esp32-hmi/lvgl_touch_gt911.h
@@ -9,13 +9,13 @@
 #define TCH_I2C_ADDR GT911_I2C_ADDR_28 // GT911 I2C address (0x28)
 #define TCH_MODE GT911_MODE_POLLING    // or GT911_MODE_INTERRUPT
 #define TCH_ROTATION initGT911::Rotate::_0
-//#define Debug_GT911_Serial
+// #define Debug_GT911_Serial
 
 #ifdef Debug_GT911_Serial
 #define TCH_Log(a) Serial.println("[GT911] " + String(a))
-#elif
+#else
 #define TCH_Log(a)
-#endif //Debug_GT911_Serial
+#endif // Debug_GT911_Serial
 
 /* initlialize the capitive touchpad */
 #include <Wire.h>
@@ -31,7 +31,11 @@ void hmi_touch_init()
   Wire.begin(I2C_SDA, I2C_SCL, 400000);
   // I2Cscanner(&Wire);
 
+#if TCH_MODE == GT911_MODE_INTERRUPT
   Touchscreen.begin(TCH_IRQ, -1, 400000);
+#else
+  Touchscreen.begin(-1, -1, 400000);
+#endif // TCH_MODE == GT911_MODE_INTERRUPT
   Touchscreen.setupDisplay(TFT_HOR_RES, TFT_VER_RES, TCH_ROTATION);
 }
 
